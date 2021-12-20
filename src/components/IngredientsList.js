0e60916f7c196a512e8bf0eafe8c51633c112cfe
AdnaneAbs <elabbasadnane@gmail.com>
2021-12-20 18:12:29 +0100

    Modal for the Modification of ingredients

--- src/components/IngredientsList.js
+++ src/components/IngredientsList.js
@@ -2,6 +2,26 @@ import React, { Component } from 'react'
 import Ingredient from './Ingredient'
 import Table from 'react-bootstrap/Table'
 export default class IngredientsList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ing1: {
+                id: 1,
+                Iname: "chocolat",
+                Type: "sucreries2",
+                Quantity : 2000,
+                Unit : "Kg"
+            },
+            ing2: {
+                id: 2,
+                Iname: "poulet",
+                Type: "viande",
+                Quantity : 100,
+                Unit : "Kg"
+            }
+        };
+    }
+    
     render() {
         return (
             <div>
@@ -16,7 +36,10 @@ export default class IngredientsList extends Component {
                         </tr>
                     </thead>
                     <tbody>
-                        <Ingredient />
+                        <Ingredient
+                            ing ={this.state.ing1}/>
+                        <Ingredient
+                            ing ={this.state.ing2}/>
                     </tbody>
                 </Table>   
             </div>
