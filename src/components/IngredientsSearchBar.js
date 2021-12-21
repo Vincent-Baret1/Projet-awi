45b39dec4fde964562169bd2165c6f006f1e2235
AdnaneAbs <elabbasadnane@gmail.com>
2021-12-21 15:36:13 +0100

    SearchBar pour la liste des ingrédients fini et fichier json pour tester la bd d'ingrédients

---
+++ src/components/IngredientsSearchBar.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+import SearchIcon from '@material-ui/icons/Search';
+import CloseIcon from '@material-ui/icons/Close';
+import "./IngredientsSearchBar.css"
+
+function IngredientsSearchBar({ placeholder, data }) {
+    const [filteredData, setFilteredData] = useState([]);
+    const [IngEntered, setIngEntered] = useState([]);
+
+    const handleFilter = (event) => {
+
+        const searchIng = event.target.value
+        setIngEntered(searchIng)
+
+        const newFilter = data.filter((value) => {
+            return value.Iname.toLowerCase().includes(searchIng.toLowerCase())
+        });
+        if (searchIng === "") {
+            setFilteredData([]);
+        }
+        else {
+            setFilteredData(newFilter);
+        }
+    };
+    const clearInput = () => {
+        setFilteredData([]);
+        setIngEntered("");
+    };
+    return (
+        <div className="search">
+            <div className="searchInputs">
+                <input 
+                    type="text"
+                    placeholder={placeholder}
+                    value={IngEntered}
+                    onChange={handleFilter} />
+
+                    {filteredData.length === 0 ? (
+                        <SearchIcon />
+                    ) :
+                        (<CloseIcon 
+                            id='clearBtn' 
+                            onClick={clearInput} />)
+                    }
+            </div>
+            
+            {filteredData.length != 0 && (
+                <div className='dataResult'>
+                    {filteredData.map((value, key) => {
+                        return (
+                            <div className="dataItem">
+                                <p>{value.Iname}</p>
+                            </div>
+                        );
+                    })
+                    }
+                </div>
+            )
+            }
+
+        </div>
+    );
+}
+
+export default IngredientsSearchBar
