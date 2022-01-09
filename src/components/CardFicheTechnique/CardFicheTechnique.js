7b844ab8b058d6d27403752c4f42eddbda73afb8
AdnaneAbs <elabbasadnane@gmail.com>
2022-01-09 17:39:22 +0100

    modal de CardFicheTech marche

--- src/components/CardFicheTechnique/CardFicheTechnique.js
+++ src/components/CardFicheTechnique/CardFicheTechnique.js
@@ -5,7 +5,7 @@ import firebase from "../../firebase";
 import "./CardFicheTechnique.css"
 import IngredientForm from "../IngredientForm";
 import ModalFicheTech from "../ModalFicheTech";
-import Modal from "../Modal";
+import { Modal } from "react-bootstrap";
 import DeleteFicheTech from "../DeleteFicheTech";
 
 import SearchIcon from '@material-ui/icons/Search';
@@ -45,7 +45,7 @@ function CardFicheTechnique() {
     }
     //
 
-    
+
     const [EnteteEntered, setEnteteEntered] = useState([]);
 
     const handleFilter = (event) => {
@@ -81,11 +81,28 @@ function CardFicheTechnique() {
 
 
     return (
-        <div class="cardAcceuil" 
-        style={{position:'relative',
-                marginLeft:'15%',
-                marginRight:'15%'
-                }}>
+        <div class="cardAcceuil"
+            style={{
+                position: 'relative',
+                marginLeft: '15%',
+                marginRight: '15%'
+            }}>
+
+            <Modal
+                show={ModalFicheTech}
+                onHide={() => { setModalFicheTech(false) }}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered>
+                <div>
+                    <p>
+                        Fiche technique à inserer ici
+                    </p>
+                </div>
+
+
+            </Modal>
+
             <div className="searchInputs">
                 <input
                     type="text"
@@ -101,45 +118,44 @@ function CardFicheTechnique() {
                 }
             </div>
             {
-            filteredData.map((elt) => (
-                <div>
+                filteredData.map((elt) => {
 
-                    <Modal show={ModalFicheTech}
-                        handleClose={() => setModalFicheTech(false)}>
-                        <h1> {elt[0].NomAuteur} </h1>
-                    </Modal>
-
-                    <Card
-                        bg="white"
-                        key={elt[0].id}
-                        text="Fiche Technique"
-                        style={{ width: '18rem', margin: '10px' }}
-                        //className="mb-2"
-                        //class="card"
-                        onMouseOver="visualiser la fiche"
-                    /*onClick={() => alert(elt[0].NomPlat)}*/
-                    >
-                        <Card.Header onClick={() => {
-                            setModalFicheTech(true)
-                        }}>
-                            {elt[0].NomPlat}
-                        </Card.Header>
-                        <Card.Body>
-                            <Card.Title>Nom du plat : {elt[0].NomPlat}</Card.Title>
-                            <Card.Text>
-                                Nom de l'auteur : {elt[0].NomAuteur}
-                            </Card.Text>
-                            <Card.Text>
-                                Nombre de couvert : {elt[0].NbCouvert}
-                            </Card.Text>
-                        </Card.Body>
-                        <Card.Footer>
-                            <DeleteFicheTech id={elt[1]} />
-                            <Button className="modifyBtn" ><EditIcon /></Button>
-                        </Card.Footer>
-                    </Card>
-                </div>
-            ))}
+                    return(
+                        <div>
+                            <Card
+                                props={elt[0]}
+                                bg="white"
+                                key={elt[0].id}
+                                text="Fiche Technique"
+                                style={{ width: '18rem', margin: '10px' }}
+                            //className="mb-2"
+                            //class="card"
+    
+                            /*onClick={() => alert(elt[0].NomPlat)}*/
+                            >
+                                <Card.Header onClick={() => {
+                                    setModalFicheTech(true)
+                                }}>
+                                    {elt[0].NomPlat}
+                                </Card.Header>
+                                <Card.Body>
+                                    <Card.Title>Nom du plat : {elt[0].NomPlat}</Card.Title>
+                                    <Card.Text>
+                                        Nom de l'auteur : {elt[0].NomAuteur}
+                                    </Card.Text>
+                                    <Card.Text>
+                                        Nombre de couvert : {elt[0].NbCouvert}
+                                    </Card.Text>
+                                </Card.Body>
+                                <Card.Footer>
+                                    <DeleteFicheTech id={elt[1]} />
+                                    <Button className="modifyBtn" ><EditIcon /></Button>
+                                </Card.Footer>
+                            </Card>
+                        </div>
+                    )
+                }
+                    )}
         </div>
     );
 }
